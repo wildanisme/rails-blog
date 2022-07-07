fd17b568d26bb16aaa87a98bfb621209dd3f4482
Wildan Taufiq Abdul Aziz <wildantaufiq.abdulaziz@gmail.com>
2022-07-07 22:49:28 +0700

    scaffold for article

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 
   def home
+    @articles = Article.all
   end
   def about
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # get '/hello', to: 'application#hello'
+  resources :articles
   root 'pages#home'
   get '/about', to: 'pages#about'
 end
